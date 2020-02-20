cadef53fd18fa5538f1d7d7612628885a907b55c
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2020-02-20 18:32:45 +0000

    Initial massive commit

---
+++ FarmingGame/FarmingGame/Main.cpp
@@ -0,0 +1,14 @@
+#ifdef _DEBUG
+#pragma comment(lib, "sfml-system-d")
+#pragma comment(lib, "sfml-window-d")
+#pragma comment(lib, "sfml-graphics-d")
+#else
+#pragma comment(lib, "sfml-system")
+#pragma comment(lib, "sfml-window")
+#pragma comment(lib, "sfml-graphics")
+#endif
+
+int main()
+{
+	return 0;
+}
